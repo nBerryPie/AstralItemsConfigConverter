62128a03248812825680037d9d59c9ad93f752c7
Nagisberry <nagisberry@gmail.com>
2016-11-05 10:46:54 +0900

    Update: Main

--- src/main/kotlin/com/nagisberry/aicc/Main.kt
+++ src/main/kotlin/com/nagisberry/aicc/Main.kt
@@ -11,15 +11,13 @@ import java.nio.file.Paths
 
 val gson = GsonBuilder().setPrettyPrinting().create()
 
-val items: MutableMap<String, MutableList<JsonObject>> = mutableMapOf()
-
 fun main(args: Array<String>) {
     val config = Paths.get(System.getProperty("user.dir"), "config.json").toFile().reader().let {
         gson.fromJson<Map<String, Map<String, Int>>>(it).mapValues { it.value.mapValues { it.value.toInt() } }
     }
     Paths.get(System.getProperty("user.dir"), "items.xlsx").toFile().let(WorkbookFactory::create).let { workbook ->
-        workbook["main"]?.rowIterator()?.asSequence()?.filter { it.rowNum != 0 }?.forEach {
-            JsonObject().apply {
+        val items = workbook["main"]?.rowIterator()?.asSequence()?.filter { it.rowNum != 0 }?.map {
+            (it[config["main"]?.get("file") ?: 7]?.stringCellValue ?: "default") to JsonObject().apply {
                 addProperty("id", it[config["main"]?.get("id") ?: 0]?.stringCellValue ?: "")
                 addProperty("material", it[config["main"]?.get("material") ?: 1]?.stringCellValue ?: "STICK")
                 addProperty("damage", it[config["main"]?.get("damage") ?: 2]?.numericCellValue?.toInt() ?: 0)
@@ -27,22 +25,28 @@ fun main(args: Array<String>) {
                 add("description", it[config["main"]?.get("description") ?: 4]?.stringCellValue?.split("\n")?.toJsonArray() ?: JsonArray())
                 addProperty("rarity", it[config["main"]?.get("rarity") ?: 5]?.numericCellValue?.toInt() ?: 0)
                 add("types", it[config["main"]?.get("types") ?: 6]?.stringCellValue?.split(",")?.toJsonArray() ?: JsonArray())
-            }.let { item -> addItem(it[config["main"]?.get("file") ?: 7]?.stringCellValue ?: "default", item) }
+            }
+        }?.groupBy { it.first }?.mapValues {
+            it.value.map { it.second }
         } ?: error("Main Sheet is Not Found")
-        //val elements = config.filterNot { it.key.equals("main", true) }.map { cfg ->
-        //    cfg.key to (workbook[cfg.key]?.rowIterator()?.asSequence()?.filter { it.rowNum != 0 }?.map { row ->
-        //        row[cfg.value["id"] ?: 0]?.stringCellValue to cfg.value.filter { it.key != "id" }.let {
-        //            JsonObject().apply {
-        //                it.forEach { addProperty(it.key, row[it.value]?.stringCellValue ?: "") }
-        //            }
-        //        }
-        //    }?.toMap() ?: error("${cfg.key} Sheet is Not Found"))
-        //}.toMap()
+        val elements = config.filterNot { it.key.equals("main", true) }.map { cfg ->
+            cfg.key to (workbook[cfg.key]?.rowIterator()?.asSequence()?.filter { it.rowNum != 0 }?.map { row ->
+                row[cfg.value["id"] ?: 0]?.stringCellValue to cfg.value.filter { it.key != "id" }.let {
+                    JsonObject().apply {
+                        it.forEach { addProperty(it.key, row[it.value]?.stringCellValue ?: "") }
+                    }
+                }
+            }?.toMap() ?: error("${cfg.key} Sheet is Not Found"))
+        }.toMap()
         items.map {
             JsonArray().apply {
-                it.value.forEach {
-                    add(it)
-                }
+                it.value.map {
+                    it.apply {
+                        elements.map { element ->
+                            element.value[get("id").asString]?.let { element.key to it }
+                        }.filterNotNull().forEach { add(it.first, it.second) }
+                    }
+                }.forEach { add(it) }
             } to Paths.get(System.getProperty("user.dir"), "output", "${it.key}.json")
         }.forEach { pair ->
             pair.second.apply {
@@ -55,11 +59,7 @@ fun main(args: Array<String>) {
     }
 }
 
-fun addItem(fileName: String, item: JsonObject) {
-    items.getOrPut(fileName) { mutableListOf() } += item
-}
-
-fun List<String>.toJsonArray() = JsonArray().apply { forEach { add(it) } }
+fun List<String>.toJsonArray() = JsonArray().apply { this@toJsonArray.forEach { add(it) } }
 
 inline fun <reified T: Any> Gson.fromJson(json: Reader): T = this.fromJson(json, T::class.java)
 
